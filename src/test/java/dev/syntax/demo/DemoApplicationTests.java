1024551631d07f178740c02a8b9d74ac1fe6534c
unknown <wjddydwns159@gmail.com>
2025-09-22 12:35:01 +0900

    Initial commit

---
+++ src/test/java/dev/syntax/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.syntax.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
